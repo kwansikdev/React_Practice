d176e2b9e4c3ef9a3b4cf2525d23bc0faf9bc49f
kwansikdev <kwansk0424@gmail.com>
2020-01-17 17:06:13 +0900

    Router setting

--- reactjs-books-review/src/App.js
+++ reactjs-books-review/src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import ErrorBoundary from "react-error-boundary";
+
 import "./App.css";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+
+const ErrorFallbackComponent = ({ error }) => <div>{error.message}</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={ErrorFallbackComponent}>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/signin">로그인</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/signin" component={Signin} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
